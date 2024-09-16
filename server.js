2353fb6f6a2bd299b47e80160fdfdb9aff47ce89
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2024-09-16 19:16:02 +0530

    some change

--- server.js
+++ server.js
@@ -32,3 +32,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'bad request'
+  })
+})
