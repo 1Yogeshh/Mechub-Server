f5e2ab12ead719dfce8cdba68a971d7e62876ead
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2024-10-13 08:51:58 +0530

    some changes

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors');
-const databaseconnection = require('./config/db.js');
+const cors = require('cors')
+const databaseconnection= require('./config/db.js')
 const cookieParser = require('cookie-parser');
 const router = require('./routes/userRoutes.js');
 const path = require('path');
+const fs = require('fs');
 
 dotenv.config();
 
-const app = express();
 
+const app =express();
 // Serve uploads folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(express.urlencoded({
-  extended: true
-}));
-
-// Initialize database
+  extended:true
+}))
 databaseconnection();
-
-// Allow cross-origin requests (CORS)
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://mechub.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser()); 
 
-// Define routes
+//route
 app.use('/api/auth', router);
 
 // Start the server
@@ -35,9 +33,8 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-// Handle undefined routes (404)
-app.get('*', (req, res) => {
-  res.status(404).json({
-    message: 'Route not found'
-  });
-});
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'bad request'
+  })
+})
